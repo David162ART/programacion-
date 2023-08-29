c95dff6c1f936112f635b3d38d8a469312e54bf1
David162ART <michis.001342@gmail.com>
2023-08-29 18:10:55 -0400

    version 2023-08-29_02

--- ahorcado.py
+++ ahorcado.py
@@ -1,10 +1,22 @@
 def jugar():
-  print('================================')
-  print('Bienvenido al Juego del Ahorcado')
-  print('================================')
-
-  print("Fin del Juego")
+    print("================================")
+    print("Bienvenido al juego del ahorcado")
+    print("================================")
     
-if(__name__ == "__main__"):
+    palabra_secreta = "mandarina"
+    
+    ahorcado = False
+    acerto = False
+    
+    while(not ahorcado and not acerto):
+        entrada = input  ("ingrese una letra....")
+        for letra in palabra_secreta:
+            if(entrada == letra) :
+                print(entrada)
+                
+        print("jugando....")
+        
+    print("Fin del juego")
+    
+if(__name__ == "__main__")
     jugar()
-    
